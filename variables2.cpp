597d305abe29bebbc2ba174007ef469a00e8a8c9
NicholasNPham <phamn1000@gmail.com>
2026-04-08 22:13:07 -0400

    new subsection

--- variables2.cpp
+++ variables2.cpp
@@ -2,14 +2,7 @@
 
 int main(){
 
-    int x; //declaration
-    x = 5; //assignment
-    int y = 6; //Declaration and Assignment
-    int sum = x + y;
-
-    std::cout << x << '\n';
-    std::cout << y << '\n';
-    std::cout << sum << '\n';
-
+    
+    
     return 0;
 }
