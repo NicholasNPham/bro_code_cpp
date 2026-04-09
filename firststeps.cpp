0e6e2c82d69f3d43063a3267b26ea3c3c77bd4bd
NicholasNPham <phamn1000@gmail.com>
2026-04-08 22:10:20 -0400

    Initial commit

---
+++ firststeps.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    //This is a comment
+    /*
+        This is
+        A
+        Multi-Line
+        Comment
+    */
+
+    std::cout << "I Like Food" <<  std::endl;
+    std::cout << "Food is Really Good"<<  "\n";
+    return 0;
+}
