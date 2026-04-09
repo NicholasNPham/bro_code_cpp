507a50dbca2a98742d5aab69963b39ea4ec5ea34
NicholasNPham <phamn1000@gmail.com>
2026-04-08 22:32:01 -0400

    var work

--- variables2.cpp
+++ variables2.cpp
@@ -2,7 +2,25 @@
 
 int main(){
 
+    //integer (whole number)
+    int age = 23;
+    int year = 2002;
+    int days = 7.5;
+
+    //double (numbers including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single character 19.44 time stamp
+    char grade = 'A';
+    char inital = 'B';
     
-    
+    std::cout << grade << '\n';
+
+    std::cout << price << "\n";
+
+    std::cout << days;
+
     return 0;
 }
