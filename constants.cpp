e0163117cb5fa6e211a0cd989cf54b5dffa6ea6a
NicholasNPham <phamn1000@gmail.com>
2026-04-09 23:09:08 -0400

    more constants and naming conventions

--- constants.cpp
+++ constants.cpp
@@ -5,11 +5,15 @@ int main() {
     // tells the compiler to prevent anything from modifying it
     // (read only)
 
-    double pi = 3.14159;
+    const double PI = 3.14159;
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
     std::cout << circumference << " cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
