9ccd44a8315b6f5b973a1f4d93d7127e49fb054e
NicholasNPham <phamn1000@gmail.com>
2026-04-11 10:17:28 -0400

    learning my level of typedef

--- typedef_alias.cpp
+++ typedef_alias.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 
 typedef std::vector<std::pair<std::string, int>> pairlist_t; // shortens the whole standerd string into pairlist
+typedef std::string text_t;
+typedef int number_t;
 
 int main(){
 
@@ -9,12 +11,23 @@ int main(){
     //          (alias) for another data type.
     //          New identiffier for an existing type
     //          Helps with readability and reduces typos
+    //          Use when there is a clear benefit
+    //          Replaced with 'using' (work better w/ templates)
+
 
     // instead of this
     // std::vector<std::pair<std::string, int>> pairlist;  
+
     
     // I can use this, way shorter than typing this everytime
     pairlist_t pairlist;
+
+    // my level of C++
+    text_t first_name = "Nick";
+    number_t age = 23;
+
+    std::cout << first_name <<'\n';
+    std::cout << age <<'\n';
 
     return 0;
 }
