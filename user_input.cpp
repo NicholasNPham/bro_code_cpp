769be78439581552564b28a11a2603d5e93dbced
NicholasNPham <phamn1000@gmail.com>
2026-04-11 11:23:38 -0400

    learning user input with multiple lines

--- user_input.cpp
+++ user_input.cpp
@@ -6,16 +6,23 @@
 int main()
 {
     std::string name;
+    std::string full_name;
     int age;
 
     std::cout << "What is your name?: ";
     std::cin >> name;
+
+    std::cin.ignore();
+
+    std::cout << "What is your full name?: ";
+    std::getline(std::cin, full_name);
     
     std::cout << "What is your age? ";
     std::cin >> age;
 
     std::cout << "Hello " << name << "\n";
-    std::cout << "You are " << age << " years old."23 << "\n";
+    std::cout << "Hello " << full_name << "\n";
+    std::cout << "You are " << age << " years old." << "\n";
 
     return 0;
 }
