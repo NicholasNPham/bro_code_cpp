04eca997022d3f00cdd27bc131e38724227c3910
NicholasNPham <phamn1000@gmail.com>
2026-04-11 10:23:39 -0400

    the most basic way to add

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    // arithmetic operators = return the result of a specific arithmetic operation ( + - * / )
+
+    int students = 20;
+
+    students = students + 1;
+
+    std::cout << students << '\n';
+
+    
+    return 0;
+}
