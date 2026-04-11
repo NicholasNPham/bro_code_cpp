54d80a1506c619e90781839da4dd1da3721522aa
NicholasNPham <phamn1000@gmail.com>
2026-04-11 10:13:10 -0400

    learning advanced typedef

--- typedef_alias.cpp
+++ typedef_alias.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t; // shortens the whole standerd string into pairlist
 
 int main(){
 
@@ -7,9 +10,11 @@ int main(){
     //          New identiffier for an existing type
     //          Helps with readability and reduces typos
 
+    // instead of this
+    // std::vector<std::pair<std::string, int>> pairlist;  
     
-
-
+    // I can use this, way shorter than typing this everytime
+    pairlist_t pairlist;
 
     return 0;
 }
