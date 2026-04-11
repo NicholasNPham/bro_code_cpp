600e0a1610ac4ab5ae3fa096a07e4570418bb3cb
NicholasNPham <phamn1000@gmail.com>
2026-04-11 10:09:19 -0400

    started new file

---
+++ typedef_alias.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    //typedef = reserved keywords used to create additional name
+    //          (alias) for another data type.
+    //          New identiffier for an existing type
+    //          Helps with readability and reduces typos
+
+    
+
+
+
+    return 0;
+}
