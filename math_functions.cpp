3e89f0e8d550cea769a3acb1b75bc753edb4494c
NicholasNPham <phamn1000@gmail.com>
2026-04-11 11:33:17 -0400

    added functions for math

---
+++ math_functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    double x = 3.99;
+    double y = 4;
+    double z;
+
+    // References can be found here: https://www.cplusplus.com/reference/cmath/
+
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 3);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // z = round(x);
+    // z = ceil(x); // rounds up 
+    // z = floor(x); // rounds down
+
+    std::cout << z << "\n";
+
+    return 0;
+}
