52fdd08ef53aab589fae155589720e47d97afc50
NicholasNPham <phamn1000@gmail.com>
2026-04-15 21:13:22 -0400

    while loop

---
+++ while_loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+
+    while(name.empty()){
+        std::cout << "Enter Your Name: ";
+        std::getline(std::cin, name);
+
+    }
+
+    std::cout << "Hello " << name << "\n";
+
+    return 0;
+}
