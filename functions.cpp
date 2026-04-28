fb3deada3aa776931e80ec62cf4046a339192337
NicholasNPham <phamn1000@gmail.com>
2026-04-27 20:08:34 -0400

    happy bday

---
+++ functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void happy_birthday(){
+    std::cout << "Happy Birthday to You!\n";
+    std::cout << "Happy Birthday to You!\n";
+    std::cout << "Happy Birthday Dear You!\n";
+    std::cout << "Happy Birthday to You!\n\n";
+}
+
+
+int main()
+{
+    // function = a block of resuable code
+
+    happy_birthday();
+    happy_birthday();
+    happy_birthday();
+
+    return 0;
+}
